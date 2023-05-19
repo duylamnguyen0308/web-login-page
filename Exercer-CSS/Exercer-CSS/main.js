15e62c8ad0ade5024c6842dfff32c6a7bd5ee304
duylamnguyen0308 <nguyenduylam03082002gmail.com>
2023-05-19 08:41:22 +0700

    hoan thanh bai tap tuan 2

---
+++ Exercer-CSS/Exercer-CSS/main.js
@@ -0,0 +1,14 @@
+function Cbutton(){
+    document.getElementById("myForm").reset();
+} 
+
+
+var dropp = document.getElementById("center")
+function setting(){
+    dropp.style.display = "none";
+}
+
+
+function Block(){
+    dropp.style.display = "block"
+}
