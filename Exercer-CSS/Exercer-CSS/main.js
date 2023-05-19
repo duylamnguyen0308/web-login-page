3f5aa54088b67cc169137408ad95f78c29623f2e
duylamnguyen0308 <nguyenduylam03082002gmail.com>
2023-05-19 11:05:47 +0700

    login add

--- Exercer-CSS/Exercer-CSS/main.js
+++ Exercer-CSS/Exercer-CSS/main.js
@@ -1,14 +1,25 @@
-function Cbutton(){
-    document.getElementById("myForm").reset();
-} 
 
-
-var dropp = document.getElementById("center")
-function setting(){
-    dropp.style.display = "none";
-}
-
-
-function Block(){
-    dropp.style.display = "block"
+async function login(event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let userinfor = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            // expiresInMins: 60, // optional
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            return data;
+        });
+    console.log(userinfor.token);
+    localStorage.setItem("login token", userinfor.token);
+    if (userinfor.token.length !==0) {
+        location.assign("index.html");
+    }
 }
